6741863d883d13ceb6839b6733733d4a834911f8
Renan Guedes <renanguedes@eprofissoes.com.br>
2026-01-15 18:34:00 -0300

    feat(database): adiciona variáveis de ambiente para deploy

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
 
 # Configuração do banco
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
+
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL") # Variável de ambiente para o banco de dados
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # A secret key está aqui apenas como lembrete. Sei que não é seguro deixá-la exposta aqui.
