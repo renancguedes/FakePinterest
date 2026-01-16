e7a5c455f4bacd06d019834749fa81b1b91c6fb9
Renan Guedes <renanguedes@eprofissoes.com.br>
2026-01-16 15:32:17 -0300

    feat(config): atualiza secret key para utilizar variável de ambiente

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -11,9 +11,7 @@ app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL") # Variável de ambiente para o banco de dados
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# A secret key está aqui apenas como lembrete. Sei que não é seguro deixá-la exposta aqui.
-app.config["SECRET_KEY"] = "719fd842589c207174e91e4d3bde6ebc"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")  # Variável de ambiente da secret key
 
 # Quando um usuário fizer o upload de uma foto, ela será salva nessa pasta
 app.config['UPLOAD_FOLDER'] = 'static/fotos_posts'
